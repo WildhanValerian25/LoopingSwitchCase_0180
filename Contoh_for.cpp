b82450e0072b638730cfb11ad7f78940701328e3
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-03-17 15:34:03 +0700

    Menambahkan program perulangan for pencacah naik dan pencacah turun

--- Contoh_for.cpp
+++ Contoh_for.cpp
@@ -9,8 +9,25 @@
     int perulanganWhile = 0;
     int perulanganDo = 0;
  
+    // Perulangan dengan for
 
- }
+    cout << "Perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n = " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
+
+    cout << "Perulangan for pencacah turun" << endl;
+    // Perulangan dengan for
+    for (n = 5; n > 0; n--)
+    {
+        cout << "Nilai n = " << n << " Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n;
+    cout << endl;
+ 
 
     
 
