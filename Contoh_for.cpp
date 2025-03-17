3033eaa48acd7e4725553dd217905a208c5b22cd
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-03-17 15:20:53 +0700

    Menambahkan Using namespace

---
+++ Contoh_for.cpp
@@ -0,0 +1,17 @@
+ #include <iostream>
+ #include <ctime>
+ #include <cstdlib>
+ using namespace std;
+
+ int main()
+ {// Deklarasi Variable
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+ 
+
+ }
+
+    
+
+   
